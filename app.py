fe753cfc787636c20035619ebf5fb3d26db049e8
sup3rbi3l <135216652+sup3rbi3l@users.noreply.github.com>
2025-05-27 22:03:10 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, request, redirect, url_for,send_file,after_this_request
+import os
+import mysql.connector
+
+app = Flask(__name__)
+def databaseConect():
+    sql = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password='',
+        database ='estoque'
+    )
+    return sql
+sql = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password=''
+    )
+
+data = sql.cursor()
+
+data.execute('SELECT COUNT(*) FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = "estoque";')
+result = data.fetchone()[0]
+
+if result > 0:
+    sql.close()
+    sql = databaseConect()
+    print('Banco encontrado')
+
+else:
+    print('chegou foda')
+    data.execute('CREATE DATABASE estoque;')
+    sql.commit()
+    sql = databaseConect()
+    data = sql.cursor()
+    data.execute("CREATE TABLE produtos (id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255), preco DECIMAL(10, 2),quantidade INT);")
+    sql.commit()
+    sql.close()
+
+@app.route('/produtos')
+def produtos():
+    sql = databaseConect()
+    data = sql.cursor()
+    data.execute('SELECT * FROM produtos')
+    table_data = data.fetchall()
+   
+    
+    return render_template('produtos.html',table_data=table_data)
+
+
+@app.route('/delete_product/<id>',methods=['GET', 'POST'])
+def delete_product(id):
+    print(id)
+    sql = databaseConect()
+    data = sql.cursor()
+    data.execute("""
+        DELETE FROM produtos
+        WHERE id = %s;
+    """,(id,))
+    
+    sql.commit()
+    sql.close
+    
+    return redirect(url_for('produtos'))
+    
+@app.route('/add_products',methods=["POST","GET"])
+def add_products():
+    
+    name = request.form.get('name')
+    price = request.form.get('price')
+    quantity = request.form.get('quantity')
+    
+    print(name,price,quantity)
+    if name == None or name == '':
+        return render_template('add_produtos.html',error='erro') 
+    
+    
+    
+    sql = databaseConect()
+    data = sql.cursor()
+    data.execute('INSERT INTO produtos(nome,preco,quantidade) VALUES (%s,%s,%s)',(name,price,quantity))
+    
+    sql.commit()
+    sql.close()
+    
+    return render_template('add_produtos.html')
+
+@app.route('/update_product/<id>',methods=['GET', 'POST'])
+def update_product(id):
+    name = request.form.get('name')
+    price = request.form.get('price')
+    quantity = request.form.get('quantity')
+    
+    
+    
+    print(name,price,quantity)
+    if request.method == 'POST':
+        
+        if name == None or name == '':
+            return render_template('add_produtos.html',error='erro') 
+    
+        sql = databaseConect()
+        data = sql.cursor()
+        data.execute('UPDATE produtos SET nome = %s,  preco = %s, quantidade = %s WHERE id = %s;',(name,price,quantity,id))
+        
+        sql.commit()
+        sql.close()
+        
+        return redirect(url_for('produtos'))
+        
+    return render_template('update_product.html',id = id)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # usa a porta definida pelo Railway
+    app.run(host='0.0.0.0', port=port, debug=True)
---
+++ teste.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+table_data = [['id','nome','preço','quantidade'],
+               ['1','bazuca','R$5','10'],
+               ['2','canhão','R$10','40']
+               ]
+
+for itens in table_data:
+    
+    print(itens[0])
+    print(itens[1])
+    print(itens[2])
+    print(itens[3])
